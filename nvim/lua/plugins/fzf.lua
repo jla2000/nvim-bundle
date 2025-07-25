10a633bd59434363758e761658b4c171f06279b6
Jan Lafferton <jan.lafferton@vector.com>
2025-07-25 14:09:01 +0200

    typedef keymaps

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -7,9 +7,10 @@ return {
     { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Recent files" },
     { "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Search files" },
     { "<leader>sR", "<cmd>FzfLua resume<cr>", desc = "Search files" },
-    { "grr", "<cmd>FzfLua lsp_references<cr>", desc = "Lsp references" },
-    { "gd", "<cmd>FzfLua lsp_definitions<cr>", desc = "Lsp definitions" },
     { "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "Find symbol" },
+    { "grr", "<cmd>FzfLua lsp_references<cr>", desc = "Lsp references" },
+    { "grt", "<cmd>FzfLua lsp_typedefs<cr>", desc = "Lsp references" },
+    { "gd", "<cmd>FzfLua lsp_definitions<cr>", desc = "Lsp definitions" },
     {
       "<leader>sS",
       function()
