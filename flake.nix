358abf9bc90e0162996757293be86de8a538ed8d
Lafferton, Jan <jan.lafferton@vector.com>
2025-10-07 09:48:14 +0200

    Use vim.pack

--- flake.nix
+++ flake.nix
@@ -7,79 +7,35 @@
       url = "github:neovim/neovim";
       flake = false;
     };
-    fff = {
-      url = "github:dmtrKovalenko/fff.nvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-appimage = {
       url = "github:ralismark/nix-appimage";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, fff, nix-appimage, neovim, ... }:
+  outputs = inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
+      pkgs = import inputs.nixpkgs {
         inherit system;
       };
-      fff-nvim = fff.packages.${system}.fff-nvim;
-      neovim-unwrapped = pkgs.neovim-unwrapped.overrideAttrs {
+      neovim-dev = pkgs.neovim-unwrapped.overrideAttrs {
         version = "0.12.0-dev";
-        src = neovim;
-      };
-      neovim-wrapped = pkgs.wrapNeovimUnstable neovim-unwrapped {
-        viAlias = true;
-        vimAlias = true;
-        plugins = with pkgs.vimPlugins; [
-          LibDeflate-nvim
-          lz-n
-          nvim-treesitter.withAllGrammars
-          { plugin = blink-cmp; optional = true; }
-          { plugin = catppuccin-nvim; optional = true; }
-          { plugin = conform-nvim; optional = true; }
-          { plugin = crates-nvim; optional = true; }
-          { plugin = diffview-nvim; optional = true; }
-          { plugin = everforest; optional = true; }
-          { plugin = fff-nvim; optional = true; }
-          { plugin = fidget-nvim; optional = true; }
-          { plugin = flash-nvim; optional = true; }
-          { plugin = fzf-lua; optional = true; }
-          { plugin = gitsigns-nvim; optional = true; }
-          { plugin = lazydev-nvim; optional = true; }
-          { plugin = live-rename-nvim; optional = true; }
-          { plugin = nerdy-nvim; optional = true; }
-          { plugin = nvim-autopairs; optional = true; }
-          { plugin = nvim-lint; optional = true; }
-          { plugin = nvim-lspconfig; optional = true; }
-          { plugin = nvim-surround; optional = true; }
-          { plugin = nvim-treesitter-context; optional = true; }
-          { plugin = nvim-treesitter-textobjects; optional = true; }
-          { plugin = oil-nvim; optional = true; }
-          { plugin = persistence-nvim; optional = true; }
-          { plugin = plantuml-nvim; optional = true; }
-          { plugin = render-markdown-nvim; optional = true; }
-          { plugin = snacks-nvim; optional = true; }
-          { plugin = symbol-usage-nvim; optional = true; }
-          { plugin = tokyonight-nvim; optional = true; }
-          { plugin = vim-tmux-navigator; optional = true; }
-          { plugin = which-key-nvim; optional = true; }
-        ];
+        src = inputs.neovim;
       };
     in
     {
       packages.${system} = rec {
-        neovim = neovim-wrapped.override {
+        neovim-bundle = pkgs.wrapNeovimUnstable neovim-dev {
           luaRcContent = ''
             vim.opt.rtp:prepend("${./nvim}")
             dofile("${./nvim/init.lua}")
           '';
         };
-        neovim-noconfig = neovim-wrapped.override { wrapRc = false; };
-        neovim-appimage = nix-appimage.lib.${system}.mkAppImage {
-          program = "${neovim}/bin/nvim";
+        neovim = neovim-dev;
+        neovim-appimage = inputs.nix-appimage.lib.${system}.mkAppImage {
+          program = "${neovim-bundle}/bin/nvim";
         };
-        default = neovim;
       };
 
       homeManagerModules.neovim = { config, lib, ... }: {
@@ -89,7 +45,7 @@
         };
 
         config = {
-          home.packages = [ (neovim-wrapped.override { wrapRc = false; }) ];
+          home.packages = [ neovim-dev ];
           xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink config.neovim.configPath;
         };
       };
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,51 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+
+vim.pack.add({
+  "https://github.com/2KAbhishek/nerdy.nvim",
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+  "https://github.com/Wansmer/symbol-usage.nvim",
+  "https://github.com/christoomey/vim-tmux-navigator",
+  "https://github.com/dmtrKovalenko/fff.nvim",
+  "https://github.com/folke/flash.nvim",
+  "https://github.com/folke/lazydev.nvim",
+  "https://github.com/folke/persistence.nvim",
+  "https://github.com/folke/snacks.nvim",
+  "https://github.com/folke/tokyonight.nvim",
+  "https://github.com/folke/which-key.nvim",
+  "https://github.com/ibhagwan/fzf-lua",
+  "https://github.com/j-hui/fidget.nvim",
+  "https://github.com/kylechui/nvim-surround",
+  "https://github.com/kylechui/nvim-surround",
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/lumen-oss/lz.n",
+  "https://github.com/mfussenegger/nvim-lint",
+  "https://github.com/neanias/everforest-nvim",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/nvim-treesitter/nvim-treesitter-context",
+  "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+  "https://github.com/saecki/crates.nvim",
+  "https://github.com/saecki/live-rename.nvim",
+  "https://github.com/sindrets/diffview.nvim",
+  "https://github.com/stevearc/conform.nvim",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/windwp/nvim-autopairs",
+  { src = "https://github.com/Saghen/blink.cmp", version = "v1.7.0" },
+  { src = "https://github.com/catppuccin/nvim", name = "catppuccin-nvim" },
+}, { load = function() end })
+
+vim.cmd.packadd("lz.n")
 require("lz.n").load("plugins")
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(event)
+    if event.data.updated then
+      vim.cmd.packadd("fff.nvim")
+      require("fff.download").download_or_build_binary()
+    end
+  end,
+})
 
 vim.cmd.colorscheme("catppuccin-macchiato")
