a752a9676aceae45b1cdbed0670ad388c67270ca
Lafferton, Jan <jan.lafferton@vector.com>
2025-10-07 15:11:05 +0200

    Add devicons

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,7 @@ vim.pack.add({
   "https://github.com/mfussenegger/nvim-lint",
   "https://github.com/neanias/everforest-nvim",
   "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/nvim-treesitter/nvim-treesitter",
   "https://github.com/nvim-treesitter/nvim-treesitter-context",
   "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
@@ -37,17 +38,8 @@ vim.pack.add({
 
 vim.cmd.packadd("lz.n")
 vim.cmd.packadd("nvim-treesitter")
+vim.cmd.packadd("nvim-web-devicons")
 
 require("lz.n").load("plugins")
-
-vim.api.nvim_create_autocmd("PackChanged", {
-  callback = function(event)
-    if event.data.updated then
-      vim.cmd.TSUpdate()
-      vim.cmd.packadd("fff.nvim")
-      require("fff.download").download_or_build_binary()
-    end
-  end,
-})
 
 vim.cmd.colorscheme("catppuccin-macchiato")
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
     after = function()
       ---@diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "rust", "zig", "nix", "toml" },
         auto_install = true,
         highlight = {
           enable = true,
