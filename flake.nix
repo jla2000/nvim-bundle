24bf076db9cf0fc151fb57b65c76d1ab0f94082b
Lafferton, Jan <jan.lafferton@vector.com>
2025-12-11 17:09:19 +0100

    Allow other platforms

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "git+https://gitlab.vi.vector.int/common/third-party/flake-utils";
     neovim = {
       url = "github:neovim/neovim";
       flake = false;
@@ -10,30 +11,30 @@
   };
 
   outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      neovim-unstable = pkgs.neovim-unwrapped.overrideAttrs {
-        version = "0.12.0-dev";
-        src = inputs.neovim;
-      };
-    in
-    {
-      packages.${system} = rec {
-        neovim-bundle = pkgs.wrapNeovimUnstable neovim-unstable {
-          luaRcContent = ''
-            vim.opt.rtp:prepend("${./nvim}")
-            dofile("${./nvim/init.lua}")
-          '';
-          vimAlias = true;
-          viAlias = true;
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        neovim-unstable = pkgs.neovim-unwrapped.overrideAttrs {
+          version = "0.12.0-dev";
+          src = inputs.neovim;
         };
-        neovim = pkgs.wrapNeovimUnstable neovim-unstable {
-          wrapRc = false;
-          vimAlias = true;
-          viAlias = true;
+      in
+      {
+        packages.${system} = rec {
+          neovim-bundle = pkgs.wrapNeovimUnstable neovim-unstable {
+            luaRcContent = ''
+              vim.opt.rtp:prepend("${./nvim}")
+              dofile("${./nvim/init.lua}")
+            '';
+            vimAlias = true;
+            viAlias = true;
+          };
+          neovim = pkgs.wrapNeovimUnstable neovim-unstable {
+            wrapRc = false;
+            vimAlias = true;
+            viAlias = true;
+          };
+          default = neovim-bundle;
         };
-        default = neovim-bundle;
-      };
-    };
+      });
 }
